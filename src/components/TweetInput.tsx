a81ce6f8d0fc583b802acee73d9915c0efa954e2
ShuheiTakahashi1224 <shuheytakahashi@gmail.com>
2022-07-24 09:39:35 +0900

    avatar 画像表示

--- src/components/TweetInput.tsx
+++ src/components/TweetInput.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
+import { auth } from "../firebase";
+import { Avatar } from "@material-ui/core";
+import styles from "./TweetInput.module.css";
 
 const TweetInput = () => {
-  return <div></div>;
+  const user = useSelector(selectUser);
+  return (
+    <div>
+      <Avatar
+        className={styles.tweet_avatar}
+        src={user.photoUrl}
+        onClick={async () => {
+          await auth.signOut();
+        }}
+      />
+    </div>
+  );
 };
 
 export default TweetInput;
